a4b439aca4f7f815713411e2091e47ea27f633a4
unetka <99989935+unetka@users.noreply.github.com>
2022-05-28 13:47:21 +0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.menu-burger__header').click(function() {
-        $('.menu-burger__header,.menu').toggleClass('open-menu');
+        $('.menu-burger__header,.menu,.logo').toggleClass('open-menu');
         $('body').toggleClass('fixed-page');
     });
 });
